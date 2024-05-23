70a32948af0df6d87a4e4947d47ecbaefd7767cf
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-05-23 18:22:41 +0600

    server setup done

---
+++ src/app/app.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { productRoutes } from './app/modules/Product/product.route';
+import { orderRoutes } from './app/modules/Order/order.route';
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+// "Not Found" route middleware
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
+
+export default app;
